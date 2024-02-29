9fd666c0d0c4afd049e8d42d627c9f505dab7530
Tee-Mig <miguelfaucheux@hotmail.com>
2024-02-29 14:24:53 +0100

    fix bug

--- server-pong/src/gateway/back.ts
+++ server-pong/src/gateway/back.ts
@@ -32,7 +32,7 @@ export class MyGateway implements OnModuleInit {
 	colorPlayer2: string = "#FF14FB"; // *couleur joueur 2
 	colorBall: string = "#FF14FB"; // *couleur de la ball
 	colorScoreAndCenterLine: string = "#FF14FB"; // *couleur du score et de la ligne du milieu
-	endScore: number = 1; // *score avant la fin d'une partie
+	endScore: number = 11; // *score avant la fin d'une partie
 	ballDirectionBeginning: number = 1; // *dans quelle direction va la balle au debut
 
 	gameData: GameData[] = []; // *store all games of Pong
